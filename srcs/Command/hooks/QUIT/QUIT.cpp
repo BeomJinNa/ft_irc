2288947f5601ebaee98a59a26c8e1b023ae924cf
Beomjin Na <bena@c2r8s6.42seoul.kr>
2024-01-30 22:27:26 +0900

    FIX: Fix QUIT command by IRC protocol

--- srcs/Command/hooks/QUIT/QUIT.cpp
+++ srcs/Command/hooks/QUIT/QUIT.cpp
@@ -1,3 +1,4 @@
+#include <set>
 #include "UserDB.hpp"
 #include "ChannelDB.hpp"
 #include "Message.hpp"
@@ -7,26 +8,31 @@ void	HookFunctionQuit(const Message& message)
 	UserDB&				userDB = UserDB::GetInstance();
 	ChannelDB&			channelDB = ChannelDB::GetInstance();
 	int					userId = message.GetUserId();
-	const std::string&	trailing = message.GetTrailing();
+	UserDB::ChannelList	list = userDB.GetJoinnedChannelList(userId);
+	std::set<int>		sendingUserList;
+	std::string			trailing = message.GetTrailing();
 
-	std::string	quitMessage;
 	if (trailing.empty())
 	{
-		quitMessage = userDB.GetNickName(userId) + " has quit.";
-	}
-	else
-	{
-		quitMessage = userDB.GetNickName(userId) + " :" + trailing;
+		trailing = userDB.GetNickName(userId) + " has quit.";
 	}
 
-	//TODO
-	//텍스트를 IRC 규격에 맞게 수정해야 함
-	UserDB::ChannelList	list = userDB.GetJoinnedChannelList(userId);
+	//:NickName!UserName@Host QUIT :trailing(message)
+	std::string	quitMessage
+		= ":" + userDB.GetNickName(userId)
+		+ "!" + userDB.GetUserName(userId)
+		+ " QUIT :" + trailing;
+
 	for (UserDB::ChannelList::iterator it = list.begin(); it != list.end(); ++it)
 	{
-		std::string	sendMessage = "#" + channelDB.GetChannelName(*it)
-								+ " :" + quitMessage;
-		channelDB.SendMessageToChannel(sendMessage, *it);
+		std::vector<int>	usersInChannel = channelDB.GetUserListInChannel(*it);
+		sendingUserList.insert(usersInChannel.begin(), usersInChannel.end());
+	}
+
+	for (std::set<int>::iterator it = sendingUserList.begin();
+		 it != sendingUserList.end(); ++it)
+	{
+		userDB.SendMessageToUser(quitMessage, *it);
 	}
 	userDB.DisconnectUser(userId);
 }
