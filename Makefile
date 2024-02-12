b30add5d94c1f13c9455ac824e9bb3da9fd118f3
sed-int <nouveau0309@gmail.com>
2024-02-12 15:01:51 +0900

    EDIT: Makefile and hooks header file

--- Makefile
+++ Makefile
@@ -20,6 +20,13 @@ SRCS	= srcs/main.cpp \
 		  srcs/Command/NICK/NICK.cpp \
 		  srcs/Command/PASS/PASS.cpp \
 		  srcs/Command/USER/USER.cpp \
+		  srcs/Command/PRIVMSG/PRIVMSG.cpp \
+		  srcs/Command/INVITE/INVITE.cpp \
+		  srcs/Command/KICK/KICK.cpp \
+		  srcs/Command/PART/PART.cpp \
+		  srcs/Command/PING/PING.cpp \
+		  srcs/Command/TOPIC/TOPIC.cpp \
+		  srcs/Command/QUIT/QUIT.cpp \
 		  srcs/ConstString.cpp \
 		  srcs/IndexManager/IndexManager.cpp \
 		  srcs/Server/FixedBufferArray.cpp \
--- includes/hooks.hpp
+++ includes/hooks.hpp
@@ -9,4 +9,11 @@ void	HookFunctionPass(const Message& message);
 void	HookFunctionUser(const Message& message);
 void	HookFunctionNick(const Message& message);
 void	HookFunctionPrivmsg(const Message& message);
+void	HookFunctionInvite(const Message& message);
+void	HookFunctionJoin(const Message& message);
+void	HookFunctionKick(const Message& message);
+void	HookFunctionPart(const Message& message);
+void	HookFunctionPass(const Message& message);
+void	HookFunctionPing(const Message& message);
+void	HookFunctionTopic(const Message& message);
 #endif
